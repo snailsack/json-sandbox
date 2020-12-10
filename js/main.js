0e8e2570615c691d11c61349df8d267034d002c8
Nick Railsback <nickrailsback@gmail.com>
2020-12-10 08:26:11 -0500

    Accessing double level JSON data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //JavaScript Document
 
-var theData1 = '{"first":"Nick", "last":"Railsback", "city":"Thomaston"}';
-var myObj = JSON.parse(theData1);
+var theData2 = '{"Melissa":{"age":"27","degree":"BA"}, "Nick":{"age":"31","degree":"BA"}}';
+var myObj = JSON.parse(theData2)
 console.log(myObj);
 
-document.getElementById("message").innerHTML = myObj.last;
+document.getElementById("message").innerHTML = myObj.Melissa.age;
