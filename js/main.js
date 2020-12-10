9bee1e4750708ca7701d5515471f27e56d26ebae
Nick Railsback <nickrailsback@gmail.com>
2020-12-10 13:40:00 -0500

    XHR ReadyState

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 //JavaScript Document
 
-var theData2 = '{"Melissa":{"age":"27","degree":{"High School Diploma":"Concord High School","BA":"Champlain College"}}, "Nick":{"age":"31","degree":"BA"}}';
-var myObj = JSON.parse(theData2)
-console.log(myObj);
+var xhr = new XMLHttpRequest();
 
-document.getElementById("message").innerHTML = myObj.Melissa.degree.BA;
+xhr.open('GET', 'data.json', true);
 
+xhr.send();
 
+xhr.onreadystatechange = function() {
+  console.log(xhr.readyState);
+  console.log(xhr.status);
+  console.log(xhr.statusText);
+}
