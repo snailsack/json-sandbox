43cd426fcf613f4097ca10bf3feb69303b20f87d
Nick Railsback <nickrailsback@gmail.com>
2020-12-10 12:47:20 -0500

    Accessing triple level JSON data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 //JavaScript Document
 
-var theData2 = '{"Melissa":{"age":"27","degree":"BA"}, "Nick":{"age":"31","degree":"BA"}}';
+var theData2 = '{"Melissa":{"age":"27","degree":{"High School Diploma":"Concord High School","BA":"Champlain College"}}, "Nick":{"age":"31","degree":"BA"}}';
 var myObj = JSON.parse(theData2)
 console.log(myObj);
 
-document.getElementById("message").innerHTML = myObj.Melissa.age;
+document.getElementById("message").innerHTML = myObj.Melissa.degree.BA;
+
+
