823edc568f6be8114253985802af60e3bbc2bd66
Nick Railsback <nickrailsback@gmail.com>
2020-12-10 16:22:42 -0500

    XHR status and onlad

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 //JavaScript Document
 
+// Create new object
 var xhr = new XMLHttpRequest();
-
+// Set the open status
 xhr.open('GET', 'data.json', true);
+// Ensure it's coming in as text
+xhr.responseType = 'text';
 
-xhr.send();
 
-xhr.onreadystatechange = function() {
-  console.log(xhr.readyState);
-  console.log(xhr.status);
-  console.log(xhr.statusText);
+
+// onload fired if readyState has value of 4
+xhr.onload = function(){
+  if(xhr.status === 200) {
+  var myStuff = JSON.parse(xhr.responseText);
+  console.log(myStuff);
+  }
 }
+
+// Send is typically at the end
+xhr.send();
